1083970cc807f6e16fb811330f03d221d55212bb
uttne <uttne.mkr@gmail.com>
2020-07-25 14:42:55 +0900

    出力を改行する

--- sample01/main.c
+++ sample01/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "sub.h"
 
 int main(void)
 {
-    char dest[64];
-    GetHelloWorldText(dest,64);
-    printf("%s", dest);
+    char dest[64];    
+    GetHelloWorldText(dest, sizeof(dest) / sizeof(char));
+    printf("%s\n", dest);
 }
